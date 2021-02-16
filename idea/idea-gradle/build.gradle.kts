a412a0eab3206332c29039c84ccab004a65a2fa7
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2021-02-16 15:48:13 +0300

    Make project partially compatible with Gradle configuration cache (part 5)
    Make Project.configureFormInstrumentation and Task.useAndroidConfiguration, :dependencies:android-sdk unzip tasks, :idea:idea-gradle:test task extensions compatible with configuration cache
    
    Relates to #KT-44611

--- idea/idea-gradle/build.gradle.kts
+++ idea/idea-gradle/build.gradle.kts
@@ -106,15 +106,13 @@ projectTest(parallel = false) {
     workingDir = rootDir
     useAndroidSdk()
 
-    doFirst {
-        val mainResourceDirPath = File(project.buildDir, "resources/main").absolutePath
-        sourceSets["test"].runtimeClasspath = sourceSets["test"].runtimeClasspath.filter { file ->
-            if (!file.absolutePath.contains(mainResourceDirPath)) {
-                true
-            } else {
-                println("Remove `${file.path}` from the test runtime classpath")
-                false
-            }
+    val mainResourceDirPath = File(project.buildDir, "resources/main").absolutePath
+    sourceSets["test"].runtimeClasspath = sourceSets["test"].runtimeClasspath.filter { file ->
+        if (!file.absolutePath.contains(mainResourceDirPath)) {
+            true
+        } else {
+            println("Remove `${file.path}` from the test runtime classpath")
+            false
         }
     }
 }
