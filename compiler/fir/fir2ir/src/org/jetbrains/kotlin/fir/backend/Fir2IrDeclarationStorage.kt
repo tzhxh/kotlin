e9843bae7aff27cc8f2f5d6021c73c2fa18be4bb
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-02-17 19:04:04 +0300

    FIR2IR: Copy annotations from original declarations to fake overrides

--- compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrDeclarationStorage.kt
+++ compiler/fir/fir2ir/src/org/jetbrains/kotlin/fir/backend/Fir2IrDeclarationStorage.kt
@@ -1191,7 +1191,9 @@ class Fir2IrDeclarationStorage(
     }
 
     private fun IrMutableAnnotationContainer.convertAnnotationsFromLibrary(firAnnotationContainer: FirAnnotationContainer) {
-        if ((firAnnotationContainer as? FirDeclaration)?.isFromLibrary == true) {
+        if ((firAnnotationContainer as? FirDeclaration)?.isFromLibrary == true ||
+            (firAnnotationContainer is FirCallableMemberDeclaration<*> && firAnnotationContainer.isSubstitutionOrIntersectionOverride)
+        ) {
             annotationGenerator.generate(this, firAnnotationContainer)
         }
     }
